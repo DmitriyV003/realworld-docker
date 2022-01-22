222110dff14e1eb609229bffa86cf20c8304f6b7
dmitriy <vdmitriy003@gmail.com>
2022-01-22 20:40:24 +0300

    feat: change nginx config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const makeApiReq = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={makeApiReq}>Make api request</button>
     </div>
   );
 }
